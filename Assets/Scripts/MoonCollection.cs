8e3dea1ece4be9a220911364038024ce6fccd94d
Phoder1 <alon.talmi@gmail.com>
2021-02-13 20:29:06 +0200

    Particles wow

---
+++ Assets/Scripts/MoonCollection.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MoonCollection : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem particles;
+
+    [System.Obsolete]
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        ParticleSystem ps = collision.gameObject.GetComponent<ParticleSystem>();
+        ps.Play();
+        collision.gameObject.GetComponent<Collider2D>().enabled = false;
+        collision.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        Destroy(collision.gameObject, ps.duration);
+        if (particles != null)
+            particles.emissionRate += 1;
+
+
+    }
+
+    
+}
