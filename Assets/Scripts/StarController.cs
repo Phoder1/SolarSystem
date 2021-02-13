4a05cffba4077cf0bfd6f3a5c666e79310b3f057
Phoder1 <alon.talmi@gmail.com>
2021-02-13 12:18:10 +0200

    Solar system

---
+++ Assets/Scripts/StarController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class StarController : MonoBehaviour
+{
+    [Range(0, 333)]
+    [SerializeField] private float mass;
+    [SerializeField] private Vector2 startVelocity;
+    [SerializeField] private bool updateGravity;
+    [SerializeField] private GameObject arrowPivot;
+    [SerializeField] private GameObject arrow;
+    private bool showArrow;
+    Rigidbody2D rb;
+    StarsManager starsManager;
+
+
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+        rb.mass = mass;
+    }
+    private void Start() {
+        rb.velocity = startVelocity;
+        starsManager = StarsManager._instance;
+        starsManager.stars.Add(this);
+        if (updateGravity)
+            starsManager.SetGravityEvent += SetGravity;
+
+    }
+    private void FixedUpdate() {
+        if (updateGravity)
+            UpdateGravity();
+    }
+    public void SetGravity(bool value) => updateGravity = value;
+    public void SetArrow(bool value) => showArrow = value;
+
+    private void UpdateGravity() {
+        //F = (Gravity const * mass1 * mass2) / distance * distance
+        foreach (StarController star in starsManager.stars) {
+            if (star != this) {
+                float force = (StarsManager.gravityConst * this.mass * star.mass) / Mathf.Pow(Vector2.Distance(rb.position, star.rb.position), 2);
+                Vector2 forceDirection = (star.rb.position - rb.position).normalized;
+                Vector2 forceVector = forceDirection * force;
+                rb.AddForce(forceVector);
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/StarsManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarsManager : MonoBehaviour
+{
+    public static StarsManager _instance;
+    [HideInInspector]
+    public List<StarController> stars;
+    public const float gravityConst = 9.8f;
+
+    public event Action<bool> SetGravityEvent;
+    public event Action<bool> SetArrowEvent;
+    private void Awake() {
+        if(_instance == null)
+            _instance = this;
+        else if (_instance != this)
+            Destroy(gameObject);
+        stars = new List<StarController>();
+    }
+
+    public void SetSimulationSpeed(float value) => Time.timeScale = value;
+    public void SetGravity(bool value) => SetGravityEvent?.Invoke(value);
+    public void SetArrow(bool value) => SetArrowEvent?.Invoke(value);
+}
